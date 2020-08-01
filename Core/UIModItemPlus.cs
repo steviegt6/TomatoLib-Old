26a0280e3a8e1e430f81b70518a2c47cf4545a0c
Stevie <xxlennygamerxx@gmail.com>
2020-07-31 22:57:24 -0700

    meme

---
+++ Core/UIModItemPlus.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace TomatoLib.Core
+{
+    internal class UIModItemPlus
+    {
+		//TODO: Get off your lazy butt and get this finished, Stevie. - Stevie
+		internal static void UIModItem_OverrideInitialize(HookEvents.Orig_UIModItem_Initialize orig, object instance)
+		{
+			orig(instance);
+		}
+	}
+}
--- TomatoLib.cs
+++ TomatoLib.cs
@@ -1,8 +1,22 @@
+using System.Reflection;
 using Terraria.ModLoader;
+using TomatoLib.Core;
 
 namespace TomatoLib
 {
 	public class TomatoLib : Mod
 	{
-	}
+        internal static Assembly Terraria;
+        public override void Load()
+        {
+            Terraria = Assembly.LoadFrom("C:\\Program Files (x86)\\Steam\\steamapps\\common\\tModLoader\\tModLoader.exe");
+
+            HookEvents.OverrideInitialize += UIModItemPlus.UIModItem_OverrideInitialize;
+        }
+
+        public override void Unload()
+        {
+            Terraria = null;
+        }
+    }
 }
