16c33069b3f61d9fce1d772cfb0cac1e39bcc048
Stevie <xxlennygamerxx@gmail.com>
2020-08-03 21:23:31 -0700

    Code cleanup

--- Core/Utils/Lang/LangAndLocalization.cs
+++ Core/Utils/Lang/LangAndLocalization.cs
@@ -68,22 +68,6 @@ namespace TomatoLib.Core.Utils.Lang
         /// <param name="displayName">The text displayed.</param>
         /// <param name="itemID"></param>
         /// <param name="color"></param>
-        public static void AddConfigOption(Mod mod, string internalName, string displayName, int itemID, Color color, bool itemAppearsAfterText = false)
-        {
-            ModTranslation modTranslation = mod.CreateTranslation(internalName);
-            string item = itemID > 0 ? $"[i:{itemID}]" : "";
-
-            modTranslation.SetDefault(!itemAppearsAfterText ? item + $"[c/{color.Hex3()}:{displayName}]" : $"[c/{color.Hex3()}:{displayName}]" + item);
-            mod.AddTranslation(modTranslation);
-        }
-
-        /// <summary>
-        /// Creates a ModTranslation suitable for config headers and labels (<c>[i:itemID][c/color:displayName]</c>). If you don't want an item, set <c>itemID</c> to <c>0</c>.
-        /// </summary>
-        /// <param name="internalName">The internal name of your translation. ("$Mods.YourModName.internalName")</param>
-        /// <param name="displayName">The text displayed.</param>
-        /// <param name="itemID"></param>
-        /// <param name="color"></param>
         public static void AddConfigOption(Mod mod, string internalName, string displayName, int itemID, string color, bool itemAppearsAfterText = false)
         {
             ModTranslation modTranslation = mod.CreateTranslation(internalName);
@@ -94,19 +78,15 @@ namespace TomatoLib.Core.Utils.Lang
         }
 
         /// <summary>
-        /// Creates a ModTranslation suitable for config headers and labels (<c>[i:itemID][c/color:displayName]</c>). If you don't want an item, set <c>itemID</c> to <c>""</c>.
+        /// Creates a ModTranslation suitable for config headers and labels (<c>[i:itemID][c/color:displayName]</c>). If you don't want an item, set <c>itemID</c> to <c>0</c>.
         /// </summary>
         /// <param name="internalName">The internal name of your translation. ("$Mods.YourModName.internalName")</param>
         /// <param name="displayName">The text displayed.</param>
         /// <param name="itemID"></param>
         /// <param name="color"></param>
-        public static void AddConfigOption(Mod mod, string internalName, string displayName, string itemID, Color color, bool itemAppearsAfterText = false)
+        public static void AddConfigOption(Mod mod, string internalName, string displayName, int itemID, Color color, bool itemAppearsAfterText = false)
         {
-            ModTranslation modTranslation = mod.CreateTranslation(internalName);
-            string item = itemID != "" ? $"[i:{mod.ItemType(itemID)}]" : "";
-
-            modTranslation.SetDefault(!itemAppearsAfterText ? item + $"[c/{color.Hex3()}:{displayName}]" : $"[c/{color.Hex3()}:{displayName}]" + item);
-            mod.AddTranslation(modTranslation);
+            AddConfigOption(mod, internalName, displayName, itemID, color.Hex3().ToString(), itemAppearsAfterText);
         }
 
         /// <summary>
@@ -118,11 +98,33 @@ namespace TomatoLib.Core.Utils.Lang
         /// <param name="color"></param>
         public static void AddConfigOption(Mod mod, string internalName, string displayName, string itemID, string color, bool itemAppearsAfterText = false)
         {
-            ModTranslation modTranslation = mod.CreateTranslation(internalName);
-            string item = itemID != "" ? $"[i:{mod.ItemType(itemID)}]" : "";
+            int item = 0;
 
-            modTranslation.SetDefault(!itemAppearsAfterText ? item + $"[c/{color}:{displayName}]" : $"[c/{color}:{displayName}]" + item);
-            mod.AddTranslation(modTranslation);
+            if (itemID != "")
+            {
+                int.TryParse(itemID, out item);
+            }
+
+            AddConfigOption(mod, internalName, displayName, item, color, itemAppearsAfterText);
+        }
+
+        /// <summary>
+        /// Creates a ModTranslation suitable for config headers and labels (<c>[i:itemID][c/color:displayName]</c>). If you don't want an item, set <c>itemID</c> to <c>""</c>.
+        /// </summary>
+        /// <param name="internalName">The internal name of your translation. ("$Mods.YourModName.internalName")</param>
+        /// <param name="displayName">The text displayed.</param>
+        /// <param name="itemID"></param>
+        /// <param name="color"></param>
+        public static void AddConfigOption(Mod mod, string internalName, string displayName, string itemID, Color color, bool itemAppearsAfterText = false)
+        {
+            int item = 0;
+
+            if (itemID != "")
+            {
+                int.TryParse(itemID, out item);
+            }
+
+            AddConfigOption(mod, internalName, displayName, item, color.Hex3().ToString(), itemAppearsAfterText);
         }
     }
 }
